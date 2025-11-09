e0bee6e6ff469ff852d1e957fc36c5d78da6d0fd
Abhilash222004 <abhilashmaity9@gmail.com>
2025-11-09 09:41:40 +0530

    backend

--- stock-tracker/src/main/java/com/abhilash/stocktracker/db/DBConnection.java
+++ stock-tracker/src/main/java/com/abhilash/stocktracker/db/DBConnection.java
@@ -5,8 +5,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:postgresql://localhost:5432/StockPortfolio";
-    private static final String USER = "postgres";
+    private static final String URL = "postgresql://postgres.duuzsonlgjweuwwtdzmy:Abhi@2004@aws-1-ap-southeast-1.pooler.supabase.com:6543/postgres";
+    private static final String USER = "postgres.duuzsonlgjweuwwtdzmy";
     private static final String PASSWORD = "Abhi@2004";
 
     public static Connection getConnection() throws SQLException {
